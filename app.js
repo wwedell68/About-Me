f44b6b1e84dcfa083c58bf86eb7f0755361a1ad1
Wyatt Wedell <wyattearp24@aol.com>
2019-01-31 20:52:10 -0800

    finished 5 questions and did come css

--- app.js
+++ app.js
@@ -1,5 +1,48 @@
 'use strict';
 
-var sports = prompt('Have I played more baseball or football over the years?');
-console.log(sports);
+var muscle = prompt('Have I ever torn a muscle?').toLowerCase();
+console.log(muscle);
 
+if (muscle === 'yes' || muscle === 'y') {
+  alert('You are correct');
+} else {
+  alert('Sorry but that isn\'t correct');
+}
+
+
+var state = prompt('Was I born in the state of Washington?').toLowerCase();
+console.log(state);
+
+if (state === 'no' || state === 'n') {
+  alert('Correct');
+} else {
+  alert('You are inccorect I was actually born in Iowa');
+}
+
+
+var cars = prompt('Do I drive a stickshift?').toLowerCase();
+console.log(cars);
+
+if (cars === 'yes' || cars === 'y') {
+  alert('StickShift is life!');
+} else {
+  alert('HAHA nope love driving stick to much');
+}
+
+var siblings = prompt('Do I have any siblings?').toLowerCase();
+console.log(siblings);
+
+if (siblings === 'no' || siblings === 'n') {
+  alert('Congrats you were right');
+} else {
+  alert('I kind of wish that I did have some');
+}
+
+var travel = prompt('Have I ever left the country?').toLowerCase();
+console.log(travel);
+
+if (travel === 'yes' || travel === 'y') {
+  alert('Yes I have. I went to Victoria Canada');
+} else {
+  alert('Sorry! But I actually have left the country');
+}
